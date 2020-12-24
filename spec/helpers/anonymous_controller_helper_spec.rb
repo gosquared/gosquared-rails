2d7f99732faa53f5d98e9be4a6eebfea1fcd1ec7
russellvaughan <russellvaughan@gmail.com>
2020-12-24 11:19:45 +0000

    fix failing test by updating html_reponse function

--- spec/helpers/anonymous_controller_helper_spec.rb
+++ spec/helpers/anonymous_controller_helper_spec.rb
@@ -5,31 +5,42 @@ class ApplicationController < ActionController::Base
   append_after_action :add_gosquared_script, :if => :html_response?
 
   def add_gosquared_script
-    response.body = response.body.gsub(CLOSING_HEAD_TAG, "<script type='text/javascript' async='true'>
-      var trackingCode = function(){
-        !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
-          arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+    response.body = response.body.gsub(CLOSING_HEAD_TAG, "<script>
+
+    (function() {
+    if (window._gs) return;
+    !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+    arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
     d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
     insertBefore(d,q)}(window,document,'script','_gs');
-    _gs('#{GosquaredRails.configure.site_token}'); _gs('set', 'trackLocal', true);
-    };
+    _gs('#{GosquaredRails.configure.site_token}', false);
+    #{GosquaredRails.configure.config_options}
 
-    var loadTracker;
-    loadTracker=function(){
-      if(!window._gs) {
-        trackingCode();
-        } else {
-          delete _gs;
-          trackingCode();
-        }
-        };
-        $(document).on('page:load', loadTracker)
-        $(document).on('turbolinks:load', loadTracker);
-        </script>" + "\n </body>"
-        )
+    function track() {
+      _gs('track');
+    }
+
+    document.addEventListener('page:load', track());
+
+    document.addEventListener('turbolinks:load', function(){
+      track();
+      });
+
+    document.addEventListener('turbolinks:render', function() {
+        _gs('chat', 'reattach')
+        })
+    })();
+
+  </script>" + "\n </head>"
+  )
   end
 
   def html_response?
-   response.content_type == "text/html"
- end
+    if response.respond_to?(:media_type)
+      response.media_type == 'text/html'
+    else
+      response.content_type == 'text/html'
+    end
+  end
+
 end
